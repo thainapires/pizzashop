25f80635666db63323dc857cb7dd22b08d68762c
Thainá Pires <thainaspiress@gmail.com>
2025-01-09 20:48:24 -0300

    add base of the code

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Dashboard(){
+    return (
+        <>
+            <Helmet title="Dashboard"></Helmet>
+            <h1>Dashboard</h1>
+        </>
+    )
+}
