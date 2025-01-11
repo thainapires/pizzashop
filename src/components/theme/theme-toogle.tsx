33015a7afdcf47bb60e9038e6a5acc9903e38cc0
Thainá Pires <thainaspiress@gmail.com>
2025-01-11 13:51:39 -0300

    feat: finishing ui of application

--- src/components/theme/theme-toogle.tsx
+++ src/components/theme/theme-toogle.tsx
@@ -2,10 +2,10 @@ import { Moon, Sun } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { useTheme } from "./theme-provider"
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+import { NotFound } from "./pages/404";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
-import { Dashboard } from "./pages/app/dashboard";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 import { Orders } from "./pages/orders/orders";
@@ -10,6 +11,7 @@ export const router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
+        errorElement: <NotFound />,
         children: [
             { path: '/', element: <Dashboard /> },
             { path: '/orders', element: <Orders /> },
