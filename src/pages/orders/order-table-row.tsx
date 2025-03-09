765594bb0844c727a8b008094d4683e51b4d4dcd
Thainá Pires <thainaspiress@gmail.com>
2025-03-09 02:15:19 -0300

    feat: view order details

--- src/pages/orders/order-table-row.tsx
+++ src/pages/orders/order-table-row.tsx
@@ -7,6 +7,7 @@ import { OrderDetails } from "./order-details";
 
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { useState } from "react";
 
 export interface OrderTableRowProps {
     order: {
@@ -19,50 +20,53 @@ export interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
-  return (
-    <TableRow>
-        <TableCell>
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button variant="outline" size="sm">
-                    <Search className="h-3 w-3" />
-                    <span className="sr-only">Detalhes do pedido</span>
-                    </Button>
-                </DialogTrigger>
-                <OrderDetails />
-            </Dialog>
-        </TableCell>
-        <TableCell className="font-mono text-xs font-medium">
-            {order.orderId}
-        </TableCell>
-        <TableCell className="text-muted-foreground">
-            {formatDistanceToNow(order.createdAt, {
-                locale: ptBR,
-                addSuffix: true,
-            })}
-        </TableCell>
-        <TableCell>
-            <OrderStatus status={order.status}/>
-        </TableCell>
-        <TableCell className="font-medium">{order.customerName}</TableCell>
-        <TableCell className="font-medium">
-            {order.total.toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL',
-            })}
-        </TableCell>
-        <TableCell>
-            <Button variant="outline" size="sm">
-            <ArrowRight className="mr-2 h-3 w-3" />
-                Aprovar
-            </Button>
-        </TableCell>
-        <TableCell>
-            <Button variant="ghost" size="sm">
-            <X className="mr-2 h-3 w-3" />
-                Cancelar
-            </Button>
-        </TableCell>
+  
+    const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+
+    return (
+        <TableRow>
+            <TableCell>
+                <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
+                    <DialogTrigger asChild>
+                        <Button variant="outline" size="sm">
+                        <Search className="h-3 w-3" />
+                        <span className="sr-only">Detalhes do pedido</span>
+                        </Button>
+                    </DialogTrigger>
+                    <OrderDetails open={isDetailsOpen} orderId={order.orderId}/>
+                </Dialog>
+            </TableCell>
+            <TableCell className="font-mono text-xs font-medium">
+                {order.orderId}
+            </TableCell>
+            <TableCell className="text-muted-foreground">
+                {formatDistanceToNow(order.createdAt, {
+                    locale: ptBR,
+                    addSuffix: true,
+                })}
+            </TableCell>
+            <TableCell>
+                <OrderStatus status={order.status}/>
+            </TableCell>
+            <TableCell className="font-medium">{order.customerName}</TableCell>
+            <TableCell className="font-medium">
+                {(order.total / 100).toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                })}
+            </TableCell>
+            <TableCell>
+                <Button variant="outline" size="sm">
+                <ArrowRight className="mr-2 h-3 w-3" />
+                    Aprovar
+                </Button>
+            </TableCell>
+            <TableCell>
+                <Button variant="ghost" size="sm">
+                <X className="mr-2 h-3 w-3" />
+                    Cancelar
+                </Button>
+            </TableCell>
         </TableRow>
-  )
+    )
 }
