799d03957613d7becabe4387f771f4f590c9a1f5
Thainá Pires <thainaspiress@gmail.com>
2025-03-09 18:16:01 -0300

    feat: error and 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,13 +5,14 @@ import { AuthLayout } from "./pages/_layouts/auth";
 import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
+import { Error } from "./pages/error";
 import { Orders } from "./pages/orders/orders";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
-        errorElement: <NotFound />,
+        errorElement: <Error />,
         children: [
             { path: '/', element: <Dashboard /> },
             { path: '/orders', element: <Orders /> },
@@ -24,5 +25,9 @@ export const router = createBrowserRouter([
             { path: '/sign-in', element: <SignIn /> },
             { path: '/sign-up', element: <SignUp /> },
         ]
+    },
+    {
+        path: '*',
+        element: <NotFound />
     }
 ])
